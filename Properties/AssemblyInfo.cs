e1a22d295697a9699171a50c12c8cc4b1e710aaa
Dmitriy Kudryk <doozzik@gmail.com>
2015-08-21 18:24:57 +0300

    Add assembly description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MOTD")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Shows up to 4 colored messages in chat to connected players")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MOTD")]
